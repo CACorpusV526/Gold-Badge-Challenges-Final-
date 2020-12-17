64a2516caafcc60fe86cdb70be950e4840f1f16d
CACorpusV526 <73607854+CACorpusV526@users.noreply.github.com>
2020-12-17 09:09:40 -0500

    Added Opening Menu

--- Gold Badge Challenges (Final)/CafeMenuUI.cs
+++ Gold Badge Challenges (Final)/CafeMenuUI.cs
@@ -10,7 +10,46 @@ namespace Gold_Badge_Challenges__Final_
     {
         public void Run()
         {
+            OperationsMenu();
+        }
 
+        private void OperationsMenu()
+        {
+            bool keepRunning = true;
+            while (keepRunning)
+            {
+                Console.WriteLine("Hey Boss, what would you like to do?\n" +
+                    "1. Add/Create A Meal\n" +
+                    "2. View All Current Meals Available\n" +
+                    "3. 86 A Meal\n" +
+                    "4. Exit");
+
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        CreateAMenuItem();
+                        break;
+                    case "2":
+                        ViewAllMenuItems();
+                        break;
+                    case "3":
+                        ByeByeAMeal();
+                        break;
+                    case "4":
+                        Console.WriteLine("Until Next Time, Boss");
+                        keepRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("I don't know what you are doing...\nPlease choose a number from the list.");
+                        break;
+                }
+
+                Console.WriteLine("Getting your choice...\n\nPress any key to continue...");
+                Console.ReadKey();
+                Console.Clear();
+            }
         }
     }
 }
